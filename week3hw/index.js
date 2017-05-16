7c502acce1590138cfc83171f6dbb9e723ac237c
Daniel <visual.eyes.success@gmail.com>
2017-05-16 22:15:13 +0000

    everything working1

--- week3hw/index.js
+++ week3hw/index.js
@@ -17,74 +17,43 @@ app.get('/', function(req,res){
     res.sendFile(__dirname + '/public/home.html'); 
 });
 
-//send plain text About page response
+//About page response
 app.get('/about', function(req,res){
     res.type('text/plain');
     res.send('About page');
 });
 
-// handle (delete) GET 
+// handle /delete
 app.get('/delete', function(req,res){
     let result = mushroom.delete(req.query.type);
     res.render('delete' , {type: req.query.type , result: result});
 });
 
-
-//is this where i put the add?
-app.post('/addMush', function(req, res){
-	res.type('text/html');
-	var mushtype = req.body.mush_type;
-	var mushothname = req.body.mush_other_name;
-	var mushuse=req.body.mush_use;
-	var mushfreq=req.body.mush_freq;
-	var mushdose=req.body.mush_dose;
-	var success= mushroom.addmush(mushtype, mushothname, mushuse, mushfreq, mushdose);
-// 	var footer = '<a href="/">Return to home</a>';
-	
-	if (success){
-		req.session.feedback = {'success': true, 'msg': 'successfully added ' + mushtype + ' to our records.'};
-	} else {
-		req.session.feedback = {'success': false, 'msg': 'Error: unable to add ' + mushtype + 'to our records. Check to make sure it has not already been added.'};
-	}
-
-//	res.redirect('/');
-});
-
-
-
-
-
-
-// handle POST
+// get
 app.post('/get', function(req,res){
     let header='Searching for the medicinal mushroom called ' +req.body.type;
     let found= mushroom.get(req.body.type);
-    res.render("details", {type: req.body.type, result: found, pageheader: header} );
+    res.render("details", {type: req.body.type, result: found} );
 });
 
+app.post('/add',function(req,res){
+    let completeMushroom={type : req.body.type, otherName: req.body.otherName, use: req.body.use, frequency: req.body.frequency, dosageMg:req.body.dosageMg};
+    let added= mushroom.add(completeMushroom);
+    res.render('add', {type: req.body.type, result:added});
+});
 
-// handle (add) GET 
-// app.get('/add', function(req,res){
-//     let result = mushroom.add(req.query.type,req.query.name);
-//     res.render('add' , {type: req.query.type , result: result});
-// });
-
-//above will not be a get, it will be a post?
-//post is the info sent by a webpage
-//get is retrieving the page and its displayed details
-
-
-//new form needs to have name"" 
-
-// define 404 handler
-//later will make this go to a page
+// 404 handler
 app.use(function(req,res) {
     res.type('text/html'); 
     res.status(404);
     res.sendFile(__dirname + '/public/404.html'); 
 });
 
-
+// 	if (success){
+// 		req.session.feedback = {'success': true, 'msg': 'successfully added ' + mushtype + ' to our records.'};
+// 	} else {
+// 		req.session.feedback = {'success': false, 'msg': 'Error: unable to add ' + mushtype + 'to our records. Check to make sure it has not already been added.'};
+// 	}
 
 app.listen(app.get('port'), function() {
     console.log('Express started');    
--- week3hw/lib/mushroom.js
+++ week3hw/lib/mushroom.js
@@ -16,7 +16,6 @@ exports.get=(type)=>{
     });
 }; 
 
-
 //delete function
 exports.delete=(type)=>{
     const oldLength = mushrooms.length;
@@ -27,45 +26,66 @@ exports.delete=(type)=>{
     return {deleted:oldLength !== mushrooms.length ,  total: mushrooms.length };
  };
 
+//add and check new list length to measure success, else return false 
 // exports.add=(newType, newOtherName, newUse, newFrequency, newDosageMg)=>{
 //     var newMushroom={type:newType.toLowerCase(), otherName:newOtherName, use:newUse, frequency:newFrequency, dosageMg:parseInt(newDosageMg) };
 //     {added: mushrooms.push(newMushroom)}
 //     return newMushroom.type + " has been added to the mushroom medicine list" +"<br>"+ mushrooms.length + " mushrooms in catalogue now";
 // };
 
+
 //hw4needs this below
 exports.add=(newMushroom)=>{
-    
-	var alreadyExists = this.get(newMushroom.type);
-	if (!alreadyExists) {
-        mushrooms.push(newMushroom);
+    var mushroomlen= mushrooms.length;
+    var mType=newMushroom.type; 
+    var mON=newMushroom.otherName;
+    var mUse=newMushroom.use;
+    var mFreq=newMushroom.frequency;
+    var mDose= newMushroom.dosageMg;
+    var completeMushroom = { type: mType, otherName: mON, use: mUse , frequency: mFreq, dosageMg: mDose};
+    mushrooms.push(completeMushroom);
+    var added =(mushrooms.length==mushroomlen) ? "" : "added";
+		
+// 	});
+    return {"action": added,  total: mushrooms.length };
 	};
 
-    return {added: !alreadyExists ,  total: mushrooms.length };
-    
-};
-    // var mType=newMushroom.type; 
-    // var mON=newMushroom.otherName;
-    // var mUse=newMushroom.use;
-    // var mFreq=newMushroom.frequency;
-    // var mDose= newMushroom.dosageMg;
-//    var completeMushroom = { type: mType, otherName: mON};
-    // , use: mUse , frequency: mFreq, dosageMg: mDose
+	
 
-exports.addMush=function(mushtype, mushothname, mushuse, mushfreq, mushdose){
-		var oldLength = mushrooms.length;
-		var newMush = {'type': type.toLowerCase(), 'engine': engine, 'users': 1};
 
-		//check to make sure mushroom isn't on the list already
-		var alreadyExists = mushrooms.find(function(item){
-			return item.type == type;
-		});
 
-		//add and check new list length to measure success, else return false 
-		if (!alreadyExists){
-			var newLength = mushrooms.push(newMush);
-			return newLength > oldLength;
-		} else { //Mushroom has already been added.
-			return false;
-		}
-	};
+
+
+
+
+
+// 	if (!alreadyExists) {
+//         mushrooms.push(completeMushroom);
+//         return {added: !alreadyExists ,  total: mushrooms.length };
+// 	}else{
+//     return item.type == type;
+// 	}    
+// };
+
+// 	//check to make sure mushroom isn't on the list already
+// 	var alreadyExists = mushrooms.find(function(item){
+		
+// 	});
+
+
+
+
+
+
+
+
+
+
+// 	//add and check new list length to measure success, else return false 
+// 	if (!alreadyExists){
+// 	var newLength = mushrooms.push(newMush);
+// 		return newLength > oldLength;
+// 	} else { //Mushroom has already been added.
+// 		return false;
+// 	    }
+// 	};
